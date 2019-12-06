fd2229b87460acf37036e87471161883e2316389
Siobhan Boyle <siobhan.boyle@domino-uk.com>
2019-12-06 09:42:22 +0000

    Check_out function works now! :D

--- Equipment_System/testsqlpost.py
+++ Equipment_System/testsqlpost.py
@@ -3,13 +3,5 @@ import datetime
 checkin = "10/12/2019"
 date_string = "10/02/2019"
 
-print(datetime.datetime.now())
-print(datetime.datetime.now() + datetime.timedelta(days=31))
-print(datetime.date.today())
-
-
-check_in_date = datetime.datetime.strptime(date_string, "%d/%m/%Y")
-formatted_check_in_date = (check_in_date.strftime("%d/%m/%Y"))
-
-print(check_in_date)
-print(formatted_check_in_date)
+check_out_date = datetime.datetime.now().strftime("%d/%m/%Y")
+print(check_out_date)
