04544e9f3358376558eb6650ad8b8a9d216f7d1f
Siobhan Boyle <siobhan.boyle@domino-uk.com>
2019-12-13 15:03:40 +0000

    Added the All Items screen and made the button click through from available items.

--- Equipment_System/UI_Wrangler_homemade.py
+++ Equipment_System/UI_Wrangler_homemade.py
@@ -1,5 +1,14 @@
 from available_items_UI import Ui_MainWindow
+from all_items import Ui_all_itemsMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+"""
+
+def openWindow(self):
+    self.window = QtWidgets.QMainWindow()
+    self.ui = Ui_all_itemsMainWindow
+    self.ui.setupUI(self.window)
+    self.window.show()"""
 
 if __name__ == "__main__":
     import sys
